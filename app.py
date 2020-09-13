127d082f04b62bd383e9464a1c7dd608c1bfaddc
shashankreddybanda <shashank24140544@gmail.com>
2020-09-13 13:57:55 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import redis
+from rq import Queue
+
+import time
+
+app = Flask(__name__)
+
+r = redis.Redis()
+q = Queue(connection=r)
+
+
+def backgroundTask(n):
+
+    delay = 2
+    
+    print("task running")
+    print(f"simulating {delay} seconds delay")
+
+    time.sleep(delay)
+
+    print(len(n))
+    print("'''task complete'''")
+    return len(n)
+
+
+@app.route("/")
+def addTask():
+
+    if request.args.get("n"):
+
+        job = q.enqueue(backgroundTask, request.args.get("n"))
+
+        return f"Task {job.id} added to queue at {job.enqueued_at}. {len(q)} tasks are in the queue "
+
+    return "No value for n"
+
+
+if __name__ == "__main__":
+    app.run()
